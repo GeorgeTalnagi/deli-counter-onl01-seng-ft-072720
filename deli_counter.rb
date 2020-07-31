a7173c9762b25f643b90a97f430a0e8e9190b2b6
George Talnagi <gtalnagi@yahoo.com>
2020-07-31 16:35:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
 
 def line(katz_deli)
-  if array.empty?
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    array.each_with_index do |name, index|
+    katz_deli.each_with_index do |name, index|
       katz_deli.push("#{index + 1}. #{name}")
     end
     katz_deli.unshift("The line is currently:")
