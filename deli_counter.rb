6ffd25666f39faf4f17ff3c4569c8fd560b15afb
George Talnagi <gtalnagi@yahoo.com>
2020-07-31 17:04:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,6 @@ puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-
+  puts "There is nobody waiting to be served!"
+  puts "Currently serving"
 end
